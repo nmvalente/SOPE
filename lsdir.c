6b547ac4a28f3fb81d70827a37627c7ae2e6886d
francisquinha <francisquinha@gmail.com>
2016-04-13 21:17:09 +0100

    já faz parse dos files e coloca num array de structs

--- lsdir.c
+++ lsdir.c
@@ -5,16 +5,13 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <time.h>
-#include <stdbool.h>
-#include <sys/wait.h>
 
 #define MAX_LENGTH 512
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
     DIR *dirp;
     struct dirent *direntp;
     struct stat stat_buf;
@@ -22,110 +19,61 @@ int main(int argc, char *argv[])
     pid_t pid;
     int status;
 
-    // number of arguments must be 2
-    if (argc != 2)
-    {
-        fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
+    if (argc != 2) {                                                                                                    // number of arguments must be 2
+        fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
-    if ((dirp = opendir( argv[1])) == NULL)
-    {
+
+    if ((dirp = opendir(argv[1])) == NULL) {                                                                            // second argument must be a valid directory path
         perror(argv[1]);
         exit(2);
     }
 
-    // header of the file
-    FILE * file_ptr;
-    file_ptr = fopen ("files.txt", "a");
-   // fprintf(file_ptr, "inode\ttitle\t\t\taccess\t size\tlast modification\n");
+    FILE *file_ptr;                                                                                                     // header of the file
+    file_ptr = fopen("files.txt", "a");
 
-    while ((direntp = readdir( dirp)) != NULL)
-    {
-        if(strcmp(direntp->d_name, ".") == 0 || strcmp(direntp->d_name, "..") == 0){
-            printf("do_nothing\n");
+    while ((direntp = readdir(dirp)) != NULL) {                                                                         // alternative to chdir()
+        if (strcmp(direntp->d_name, ".") == 0 || strcmp(direntp->d_name, "..") == 0)
+            continue;
+        sprintf(name, "%s/%s", argv[1], direntp->d_name);
+        if (lstat(name, &stat_buf) == -1) {                                                                             // if there is some kind of error
+            perror("lstat error");
+            int error = errno;
+            if (error == EACCES)                                                                                        // if permission denied on that directory
+                fprintf(stderr, "in %s\n\n", name);
         }
-        else
-        {
-            sprintf(name,"%s/%s",argv[1],direntp->d_name);
-        // alternativa a chdir()
-            if (lstat(name, &stat_buf)==-1)
-            {
-                perror("lstat error");
-                int error = errno;
-                if(error  == EACCES){
-                    fprintf(stderr, "in %s\n\n", name);
-                }
-             //if permission denied on that directory
-            //exit(3);
-            }
-            if (S_ISREG(stat_buf.st_mode))
-            {
-                fprintf(file_ptr,"%-7ld %-23s ", (long) stat_buf.st_ino, direntp->d_name);
-                fprintf(file_ptr, (stat_buf.st_mode & S_IRUSR) ? "r" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IWUSR) ? "w" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IXUSR) ? "x" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IRGRP) ? "r" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IWGRP) ? "w" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IXGRP) ? "x" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IROTH) ? "r" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IWOTH) ? "w" : "-");
-                fprintf(file_ptr, (stat_buf.st_mode & S_IXOTH) ? "x" : "-");
-                fprintf(file_ptr," %-6lld",(long long) stat_buf.st_size);
-                fprintf(file_ptr," %s", ctime(&stat_buf.st_mtime));
-                char cwd[1024];
-                if (getcwd(cwd, sizeof(cwd)) != NULL)
-                   fprintf(file_ptr, "%s\n", cwd);
-               else
-                   perror("getcwd() error");
-           }
-           else if(S_ISDIR(stat_buf.st_mode))
-           {
-            pid=fork();
-            if(pid == 0){
-                execlp("lsdir", "lsdir", name, NULL);
-            }
+        if (S_ISREG(stat_buf.st_mode)) {                                                                                // if regular file save file info
+            fprintf(file_ptr, "%-7ld ", (long) stat_buf.st_ino);                                                        // inode
+            fprintf(file_ptr, (stat_buf.st_mode & S_IRUSR) ? "r" : "-");                                                // permissions
+            fprintf(file_ptr, (stat_buf.st_mode & S_IWUSR) ? "w" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IXUSR) ? "x" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IRGRP) ? "r" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IWGRP) ? "w" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IXGRP) ? "x" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IROTH) ? "r" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IWOTH) ? "w" : "-");
+            fprintf(file_ptr, (stat_buf.st_mode & S_IXOTH) ? "x" : "-");
+            char *dtime = ctime(&stat_buf.st_mtime);                                                                    // modification date
+            dtime[24] = 0;
+            fprintf(file_ptr, " %s", dtime);
+            fprintf(file_ptr, " %-10lld", (long long)(stat_buf.st_mtime));
+            fprintf(file_ptr, " %-9lld", (long long) stat_buf.st_size);
+            char cwd[4096];                                                                                             // path
+            if (getcwd(cwd, sizeof(cwd)) != NULL)
+                fprintf(file_ptr, " %s/%s/%s\n", cwd, argv[1], direntp->d_name);
+            else
+                perror("getcwd() error");
+        }
+        else if (S_ISDIR(stat_buf.st_mode)) {                                                                           // if directory start new lsdir process
+            pid = fork();
+            if (pid == 0)
+                execlp("./lsdir", "./lsdir", name, NULL);
             else wait(&status);
         }
     }
-}
-fclose(file_ptr);
-closedir(dirp);
-exit(0);
-}
 
-// context, name, permissions and regulars
-bool compareContextFiles(char * fname1, char * fname2){
-    if(fname1 == fname2) return true;
+    fclose(file_ptr);
+    closedir(dirp);
+    exit(0);
 
-    int ch1, ch2;
-    FILE * fp1, * fp2;
-    fp1 = fopen(fname1, "r");
-    fp2 = fopen(fname2, "r");
-
-    if (fp1 == NULL)
-    {
-      printf("Cannot open %s for reading ", fname1);
-      exit(1);
-  }
-  else if (fp2 == NULL)
-  {
-      printf("Cannot open %s for reading ", fname2);
-      exit(1);
-  }
-  else
-  {
-      ch1 = getc(fp1);
-      ch2 = getc(fp2);
-
-      while ((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2))
-      {
-         ch1 = getc(fp1);
-         ch2 = getc(fp2);
-     }
-     fclose(fp1);
-     fclose(fp2);
- }
- if (ch1 == ch2)
-     return true;
- return false;
 }
--- rmdup.c
+++ rmdup.c
@@ -2,64 +2,120 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <time.h>
 #include <stdbool.h>
+#include <libgen.h>
+
 #define MAX_LENGTH 512
 
+struct FileInfo {
+    char* name;
+    char* path;
+    time_t mtime;
+    int inode;
+    int size;
+    char permissions[10];
+};
 
-int main(int argc, char *argv[])
-{
-    DIR *dirp;
-    struct dirent *direntp;
-    struct stat stat_buf;
-    char name[MAX_LENGTH];
+int main(int argc, char *argv[]) {
 
-    // number of arguments must be 2
-    if (argc != 2)
-    {
-        fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
+    pid_t pid;
+    int status;
+    char* line;
+    char inode_str[9];
+    char time_str[11];
+    char size_str[10];
+    size_t line_length;
+    struct FileInfo* fileInfos;
+    int numberFiles = 0;
+
+    if (argc != 2) {                                                                                                    // number of arguments must be 2
+        fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
-    if ((dirp = opendir( argv[1])) == NULL)
-    {
-        perror(argv[1]);
-        exit(2);
+
+    pid = fork();
+    if (pid == 0)
+        execlp("./lsdir", "./lsdir", argv[1], NULL);
+    else wait(&status);
+
+    FILE* file_ptr;                                                                                                     // header of the file
+    file_ptr = fopen("files.txt", "r");
+
+    if (file_ptr == NULL) {
+        perror("Error opening files.txt.\n");
+        exit(EXIT_FAILURE);
     }
 
-    // header of the file
-    FILE * file_ptr;
-    file_ptr = fopen ("files.txt", "a");
-    fprintf(file_ptr, "inode\ttitle\t\t\taccess\t size\tlast modification\n");
+    while (getline(&line, &line_length, file_ptr) != -1) numberFiles++;
 
-    while ((direntp = readdir( dirp)) != NULL)
-    {
-        sprintf(name,"%s/%s",argv[1],direntp->d_name);
-        // alternativa a chdir()
-        if (lstat(name, &stat_buf)==-1)
-        {
-            perror("lstat ERROR"); //if permission denied on that directory
-            exit(3);
-        }
-        if (S_ISREG(stat_buf.st_mode)) {
-            fprintf(file_ptr,"%-7ld %-23s ", (long) stat_buf.st_ino, direntp->d_name);
-            fprintf(file_ptr, (stat_buf.st_mode & S_IRUSR) ? "r" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWUSR) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXUSR) ? "x" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IRGRP) ? "r" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWGRP) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXGRP) ? "x" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IROTH) ? "r" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWOTH) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXOTH) ? "x" : "-");
-            fprintf(file_ptr," %-6lld",(long long) stat_buf.st_size);
-            fprintf(file_ptr," %-5s", ctime(&stat_buf.st_mtime));
-        }
+    fileInfos = malloc(numberFiles * sizeof(struct FileInfo));
+    rewind(file_ptr);
+
+    int i = 0;
+    ssize_t real_lentgh;
+    while ((real_lentgh = getline(&line, &line_length, file_ptr)) != -1) {
+        line[real_lentgh - 1] = '\0';
+        printf("|%s|\n", line);
+        struct FileInfo fileInfo;
+        memcpy(inode_str, &line[0], 8);
+        inode_str[8] = '\0';
+        fileInfo.inode = atoi(inode_str);
+        printf("|%d|\n", fileInfo.inode);
+        memcpy(fileInfo.permissions, &line[9], 9);
+        fileInfo.permissions[9] = '\0';
+        printf("|%s|\n", fileInfo.permissions);
+        memcpy(time_str, &line[44], 10);
+        time_str[10] = '\0';
+        fileInfo.mtime = (time_t)(atoll(time_str));
+        printf("|%-10lld|\n", (long long) fileInfo.mtime);
+        memcpy(size_str, &line[55], 9);
+        size_str[9] = '\0';
+        fileInfo.size = atoi(size_str);
+        printf("|%d|\n", fileInfo.size);
+        fileInfo.path = malloc((int)(real_lentgh - 66) * sizeof(char));
+        memcpy(fileInfo.path, &line[65], real_lentgh - 66);
+        fileInfo.path[real_lentgh - 66] = '\0';
+        printf("|%s|\n", fileInfo.path);
+        fileInfo.name = basename(fileInfo.path);
+        printf("|%s|\n", fileInfo.name);
+        fileInfos[i] = fileInfo;
+        i++;
     }
+    
     fclose(file_ptr);
-    closedir(dirp);
     exit(0);
+}
+
+
+// context, name, permissions and regulars
+bool compareContextFiles(char *fname1, char *fname2) {
+    if (fname1 == fname2) return true;
+
+    int ch1, ch2;
+    FILE *fp1, *fp2;
+    fp1 = fopen(fname1, "r");
+    fp2 = fopen(fname2, "r");
+
+    if (fp1 == NULL) {
+        printf("Cannot open %s for reading ", fname1);
+        exit(1);
+    }
+    else if (fp2 == NULL) {
+        printf("Cannot open %s for reading ", fname2);
+        exit(1);
+    }
+    else {
+        ch1 = getc(fp1);
+        ch2 = getc(fp2);
+
+        while ((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2)) {
+            ch1 = getc(fp1);
+            ch2 = getc(fp2);
+        }
+        fclose(fp1);
+        fclose(fp2);
+    }
+    if (ch1 == ch2)
+        return true;
+    return false;
 }
