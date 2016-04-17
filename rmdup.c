54ccbe84c32911a6adf20b0642b535bb78f586a0
francisquinha <francisquinha@gmail.com>
2016-04-17 18:53:45 +0100

    pointers...

--- rmdup.c
+++ rmdup.c
@@ -16,12 +16,12 @@
 
 struct FileInfo {
     int number;
-    char *name;
-    char *path;
-    time_t mtime;
     int inode;
-    int size;
     char permissions[PERMISSIONS_LENGTH];
+    time_t mtime;
+    int size;
+    char *path;
+    char *name;
 };
 
 int runlsdir(char *directory) {
@@ -59,7 +59,7 @@ int getNumberFiles(FILE *file_ptr) {
     return numberFiles;
 }
 
-int getFileInfos(struct FileInfo *fileInfos, FILE *file_ptr) {
+int getFileInfos(struct FileInfo **fileInfos, FILE *file_ptr) {
     char inode_str[INODE_LENGTH];
     char time_str[TIME_LENGTH];
     char size_str[SIZE_LENGTH];
@@ -76,45 +76,45 @@ int getFileInfos(struct FileInfo *fileInfos, FILE *file_ptr) {
             return -1;
         }
         line[real_lentgh - 1] = '\0';
-        struct FileInfo fileInfo;
-        fileInfo.number = file_index;
+        struct FileInfo *fileInfo = malloc(sizeof(struct FileInfo));
+        fileInfo->number = file_index;
         memcpy(inode_str, &line[0], INODE_LENGTH - 1);
         inode_str[INODE_LENGTH - 1] = '\0';
-        fileInfo.inode = atoi(inode_str);
-        memcpy(fileInfo.permissions, &line[INODE_LENGTH], PERMISSIONS_LENGTH - 1);
-        fileInfo.permissions[PERMISSIONS_LENGTH - 1] = '\0';
+        fileInfo->inode = atoi(inode_str);
+        memcpy(fileInfo->permissions, &line[INODE_LENGTH], PERMISSIONS_LENGTH - 1);
+        fileInfo->permissions[PERMISSIONS_LENGTH - 1] = '\0';
         memcpy(time_str, &line[BEFORE_TIME_LENGTH], TIME_LENGTH - 1);
         time_str[TIME_LENGTH - 1] = '\0';
-        fileInfo.mtime = (time_t) (atoll(time_str));
+        fileInfo->mtime = (time_t) (atoll(time_str));
         memcpy(size_str, &line[BEFORE_TIME_LENGTH + TIME_LENGTH], SIZE_LENGTH - 1);
         size_str[SIZE_LENGTH - 1] = '\0';
-        fileInfo.size = atoi(size_str);
-        fileInfo.path = malloc((int) (real_lentgh - 66) * sizeof(char));
-        memcpy(fileInfo.path, &line[BEFORE_PATH_LENGTH], real_lentgh - BEFORE_PATH_LENGTH - 1);
-        fileInfo.path[real_lentgh - BEFORE_PATH_LENGTH - 1] = '\0';
+        fileInfo->size = atoi(size_str);
+        fileInfo->path = malloc((int) (real_lentgh - BEFORE_PATH_LENGTH) * sizeof(char));
+        memcpy(fileInfo->path, &line[BEFORE_PATH_LENGTH], real_lentgh - BEFORE_PATH_LENGTH - 1);
+        fileInfo->path[real_lentgh - BEFORE_PATH_LENGTH - 1] = '\0';
         path_index = (int) (real_lentgh - BEFORE_PATH_LENGTH - 2);
-        while (fileInfo.path[path_index]) {
-            if (fileInfo.path[path_index] == ' ') {
-                fileInfo.path[path_index] = '/';
+        while (fileInfo->path[path_index]) {
+            if (fileInfo->path[path_index] == ' ') {
+                fileInfo->path[path_index] = '/';
                 break;
             }
             path_index--;
         }
-        name = basename(fileInfo.path);
+        name = basename(fileInfo->path);
         name_length = (int) strlen(name);
-        fileInfo.name = malloc(name_length * sizeof(char));
-        memcpy(fileInfo.name, &name[0], name_length);
-        fileInfo.name[name_length] = '\0';
+        fileInfo->name = malloc(name_length * sizeof(char));
+        memcpy(fileInfo->name, &name[0], name_length);
+        fileInfo->name[name_length] = '\0';
         fileInfos[file_index] = fileInfo;
         if (LOG) {
             printf("----------------------------------------------------------------------------------------------------\n");
-            printf("file: %d\n", fileInfo.number);
-            printf("inode: %d\n", fileInfo.inode);
-            printf("permissions: %s\n", fileInfo.permissions);
-            printf("date: %-10lld\n", (long long) fileInfo.mtime);
-            printf("size: %d\n", fileInfo.size);
-            printf("path: %s\n", fileInfo.path);
-            printf("name: %s\n", fileInfo.name);
+            printf("file: %d\n", fileInfo->number);
+            printf("inode: %d\n", fileInfo->inode);
+            printf("permissions: %s\n", fileInfo->permissions);
+            printf("date: %-10lld\n", (long long) fileInfo->mtime);
+            printf("size: %d\n", fileInfo->size);
+            printf("path: %s\n", fileInfo->path);
+            printf("name: %s\n", fileInfo->name);
         }
         file_index++;
     }
@@ -125,7 +125,7 @@ int getFileInfos(struct FileInfo *fileInfos, FILE *file_ptr) {
     return 0;
 }
 
-int compareFileContents(struct FileInfo file1, struct FileInfo file2) {
+int compareFileContents(struct FileInfo *file1, struct FileInfo *file2) {
     pid_t pid;
     int status;
     int link[2];
@@ -142,45 +142,45 @@ int compareFileContents(struct FileInfo file1, struct FileInfo file2) {
         dup2(link[1], STDOUT_FILENO);
         close(link[0]);
         close(link[1]);
-        execlp("diff", "diff", file1.path, file2.path, NULL);
+        execlp("diff", "diff", file1->path, file2->path, NULL);
     } else wait(&status);
     close(link[1]);
     int nbytes = (int) read(link[0], whatever, sizeof(whatever));
     if (nbytes == 0) {
-        if (LOG) printf("Same file contents: %d, %d\n", file1.number, file2.number);
+        if (LOG) printf("Same file contents: %d, %d\n", file1->number, file2->number);
         return 0;
     }
-    if (LOG) printf("Different file contents: %d, %d\n", file1.number, file2.number);
+    if (LOG) printf("Different file contents: %d, %d\n", file1->number, file2->number);
     return 1;
 }
 
-int compareFiles(struct FileInfo file1, struct FileInfo file2) {
-    if (file1.inode == file2.inode) {
-        if (LOG) printf("Same inodes: %d, %d\n", file1.number, file2.number);
+int compareFiles(struct FileInfo *file1, struct FileInfo *file2) {
+    if (file1->inode == file2->inode) {
+        if (LOG) printf("Same inodes: %d, %d\n", file1->number, file2->number);
         return 3;                                                                                                       // same inodes return 3;
     }
-    if (strcmp(file1.name, file2.name)) {                                                                               // different file names return 2;
-        if (LOG) printf("Different file names: %d, %d.\n", file1.number, file2.number);
+    if (strcmp(file1->name, file2->name)) {                                                                             // different file names return 2;
+        if (LOG) printf("Different file names: %d, %d.\n", file1->number, file2->number);
         return 2;
     }
-    if (strcmp(file1.permissions, file2.permissions)) {                                                                 // same file name, but different permissions, return 1
-        if (LOG) printf("Different permissions: %d, %d.\n", file1.number, file2.number);
+    if (strcmp(file1->permissions, file2->permissions)) {                                                               // same file name, but different permissions, return 1
+        if (LOG) printf("Different permissions: %d, %d.\n", file1->number, file2->number);
         return 1;
     }
-    if (file1.size != file2.size) {                                                                                     // same file name, same permissions, but different sizes, return 1
-        if (LOG) printf("Different sizes: %d, %d.\n", file1.number, file2.number);
+    if (file1->size != file2->size) {                                                                                   // same file name, same permissions, but different sizes, return 1
+        if (LOG) printf("Different sizes: %d, %d.\n", file1->number, file2->number);
         return 1;
     }
     return compareFileContents(file1, file2);                                                                           // same contents return 0, different contents return 1, error return -1
 }
 
-int createHardLink(struct FileInfo file1, struct FileInfo file2) {
-    int unlink_res = unlink(file2.path);
+int createHardLink(struct FileInfo *file1, struct FileInfo *file2) {
+    int unlink_res = unlink(file2->path);
     if (unlink_res) {
         perror("Error unlinking file.\n");
         return unlink_res;
     }
-    int link_res = link(file1.path, file2.path);
+    int link_res = link(file1->path, file2->path);
     if (link_res) perror("Error linking files.\n");
     return link_res;
 }
@@ -198,7 +198,7 @@ int main(int argc, char *argv[]) {
         exit(3);
     }
     int numberFiles = getNumberFiles(file_ptr);                                                                         // determine number of files
-    struct FileInfo *fileInfos = malloc(numberFiles * sizeof(struct FileInfo));
+    struct FileInfo **fileInfos = malloc(numberFiles * sizeof(struct FileInfo*));
     if (getFileInfos(fileInfos, file_ptr))                                                                              // parse sorted file informations into array of structs
         exit(4);
     int i = 0;
