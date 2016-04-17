4efd8d286ddfe926249bf99391d7f3744d638286
francisquinha <francisquinha@gmail.com>
2016-04-17 18:03:31 +0100

    algum refactoring

--- lsdir.c
+++ lsdir.c
@@ -8,76 +8,73 @@
 #include <time.h>
 #include <sys/wait.h>
 
-#define MAX_LENGTH 512
+#define MAX_PATH_LENGTH 4096
+#define MAX_NAME_LENGTH 512
+#define DATE_LENGTH 24
 
+void saveFileInfo(char *dir_name, char *file_name, struct stat *stat_buf, FILE *file_ptr) {
+    fprintf(file_ptr, "%-8ld ", (long) (*stat_buf).st_ino);                                                             // inode
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IRUSR) ? "r" : "-");                                                     // permissions
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IWUSR) ? "w" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IXUSR) ? "x" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IRGRP) ? "r" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IWGRP) ? "w" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IXGRP) ? "x" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IROTH) ? "r" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IWOTH) ? "w" : "-");
+    fprintf(file_ptr, ((*stat_buf).st_mode & S_IXOTH) ? "x" : "-");
+    char *dtime = ctime(&(*stat_buf).st_mtime);                                                                         // modification date
+    dtime[DATE_LENGTH] = 0;
+    fprintf(file_ptr, " %s", dtime);
+    fprintf(file_ptr, " %-10lld", (long long)((*stat_buf).st_mtime));
+    fprintf(file_ptr, " %-9lld", (long long) (*stat_buf).st_size);
+    char cwd[MAX_PATH_LENGTH];                                                                                          // path and name
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        fprintf(file_ptr, " %s/%s %s\n", cwd, dir_name, file_name);
+    else
+        perror("getcwd() error");
+}
 
 int main(int argc, char *argv[]) {
-
     DIR *dirp;
     struct dirent *direntp;
     struct stat stat_buf;
-    char name[MAX_LENGTH];
+    char name[MAX_NAME_LENGTH];
     pid_t pid;
     int status;
-
     if (argc != 2) {                                                                                                    // number of arguments must be 2
         fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
-
     if ((dirp = opendir(argv[1])) == NULL) {                                                                            // second argument must be a valid directory path
         perror(argv[1]);
         exit(2);
     }
-
-    FILE *file_ptr;                                                                                                     // header of the file
-    file_ptr = fopen("files.txt", "a");
-
+    FILE *file_ptr = fopen("files.txt", "a");
     while ((direntp = readdir(dirp)) != NULL) {                                                                         // alternative to chdir()
         if (strcmp(direntp->d_name, ".") == 0 || strcmp(direntp->d_name, "..") == 0)
             continue;
         sprintf(name, "%s/%s", argv[1], direntp->d_name);
-        if (lstat(name, &stat_buf) == -1) {                                                                             // if there is some kind of error
+        if (lstat(name, &stat_buf) == -1) {                                                                             // if there is some lstat of error
             perror("lstat error");
             int error = errno;
             if (error == EACCES)                                                                                        // if permission denied on that directory
                 fprintf(stderr, "in %s\n\n", name);
         }
-        if (S_ISREG(stat_buf.st_mode)) {                                                                                // if regular file save file info
-            fprintf(file_ptr, "%-8ld ", (long) stat_buf.st_ino);                                                        // inode
-            fprintf(file_ptr, (stat_buf.st_mode & S_IRUSR) ? "r" : "-");                                                // permissions
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWUSR) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXUSR) ? "x" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IRGRP) ? "r" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWGRP) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXGRP) ? "x" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IROTH) ? "r" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IWOTH) ? "w" : "-");
-            fprintf(file_ptr, (stat_buf.st_mode & S_IXOTH) ? "x" : "-");
-            char *dtime = ctime(&stat_buf.st_mtime);                                                                    // modification date
-            dtime[24] = 0;
-            fprintf(file_ptr, " %s", dtime);
-            fprintf(file_ptr, " %-10lld", (long long)(stat_buf.st_mtime));
-            fprintf(file_ptr, " %-9lld", (long long) stat_buf.st_size);
-            char cwd[4096];                                                                                             // path
-            if (getcwd(cwd, sizeof(cwd)) != NULL)
-                fprintf(file_ptr, " %s/%s %s\n", cwd, argv[1], direntp->d_name);
-            else
-                perror("getcwd() error");
+        if (S_ISREG(stat_buf.st_mode)) {                                                                                // if regular file, save file info
+            saveFileInfo(argv[1], direntp->d_name, &stat_buf, file_ptr);
         }
         else if (S_ISDIR(stat_buf.st_mode)) {                                                                           // if directory start new lsdir process
             if ((pid = fork()) == -1) {
                 perror("Error creating fork.\n");
-                exit(EXIT_FAILURE);
+                exit(3);
             }
             if (pid == 0)
                 execlp("./lsdir", "./lsdir", name, NULL);
             else wait(&status);
         }
     }
-
     fclose(file_ptr);
     closedir(dirp);
     exit(0);
-
 }
--- rmdup.c
+++ rmdup.c
@@ -5,26 +5,32 @@
 #include <libgen.h>
 #include <sys/wait.h>
 
-#define MAX_LENGTH 512
+#define PERMISSIONS_LENGTH 10
+#define INODE_LENGTH 9
+#define TIME_LENGTH 11
+#define SIZE_LENGTH 10
+#define BEFORE_PATH_LENGTH 65
+#define BEFORE_TIME_LENGTH 44
+
+#define LOG 1
 
 struct FileInfo {
-    char* name;
-    char* path;
+    int number;
+    char *name;
+    char *path;
     time_t mtime;
     int inode;
     int size;
-    char permissions[10];
+    char permissions[PERMISSIONS_LENGTH];
 };
 
-
-int runlsdir(char* directory) {
+int runlsdir(char *directory) {
     pid_t pid;
     int status;
-    FILE* file_ptr;                                                                                                     // header of the file
-    file_ptr = fopen("files.txt", "w");
+    FILE *file_ptr = fopen("files.txt", "w");
     if (file_ptr == NULL) {
         perror("Error opening files.txt.\n");
-        return 1;
+        return -1;
     }
     fclose(file_ptr);
     if ((pid = fork()) == -1) {
@@ -34,15 +40,18 @@ int runlsdir(char* directory) {
     if (pid == 0)
         execlp("./lsdir", "./lsdir", directory, NULL);
     else wait(&status);
-    pid = fork();
+    if ((pid = fork()) == -1) {
+        perror("Error creating fork.\n");
+        return -1;
+    }
     if (pid == 0)
         execlp("sort", "sort", "files.txt", "-k", "11,11", "-k", "8,8", "-o", "files_sorted.txt", NULL);
     else wait(&status);
     return 0;
 }
 
-int getNumberFiles(FILE* file_ptr) {
-    char* line = NULL;
+int getNumberFiles(FILE *file_ptr) {
+    char *line = NULL;
     size_t line_length = 0;
     int numberFiles = 0;
     while (getline(&line, &line_length, file_ptr) != -1) numberFiles++;
@@ -50,62 +59,73 @@ int getNumberFiles(FILE* file_ptr) {
     return numberFiles;
 }
 
-int getFileInfos(struct FileInfo* fileInfos, FILE* file_ptr) {                                                          // need to add error checking
-
-    char inode_str[9];
-    char time_str[11];
-    char size_str[10];
-    char* line = NULL;
+int getFileInfos(struct FileInfo *fileInfos, FILE *file_ptr) {
+    char inode_str[INODE_LENGTH];
+    char time_str[TIME_LENGTH];
+    char size_str[SIZE_LENGTH];
+    char *line = NULL;
+    char *name;
+    int name_length;
     size_t line_length = 0;
     ssize_t real_lentgh;
     int file_index = 0;
     int path_index;
-
     while ((real_lentgh = getline(&line, &line_length, file_ptr)) != -1) {
+        if (real_lentgh < BEFORE_PATH_LENGTH + 2) {
+            printf("Error parsing file info.\n");
+            return -1;
+        }
         line[real_lentgh - 1] = '\0';
         struct FileInfo fileInfo;
-        memcpy(inode_str, &line[0], 8);
-        inode_str[8] = '\0';
+        fileInfo.number = file_index;
+        memcpy(inode_str, &line[0], INODE_LENGTH - 1);
+        inode_str[INODE_LENGTH - 1] = '\0';
         fileInfo.inode = atoi(inode_str);
-        memcpy(fileInfo.permissions, &line[9], 9);
-        fileInfo.permissions[9] = '\0';
-        memcpy(time_str, &line[44], 10);
-        time_str[10] = '\0';
-        fileInfo.mtime = (time_t)(atoll(time_str));
-        memcpy(size_str, &line[55], 9);
-        size_str[9] = '\0';
+        memcpy(fileInfo.permissions, &line[INODE_LENGTH], PERMISSIONS_LENGTH - 1);
+        fileInfo.permissions[PERMISSIONS_LENGTH - 1] = '\0';
+        memcpy(time_str, &line[BEFORE_TIME_LENGTH], TIME_LENGTH - 1);
+        time_str[TIME_LENGTH - 1] = '\0';
+        fileInfo.mtime = (time_t) (atoll(time_str));
+        memcpy(size_str, &line[BEFORE_TIME_LENGTH + TIME_LENGTH], SIZE_LENGTH - 1);
+        size_str[SIZE_LENGTH - 1] = '\0';
         fileInfo.size = atoi(size_str);
-        fileInfo.path = malloc((int)(real_lentgh - 66) * sizeof(char));
-        memcpy(fileInfo.path, &line[65], real_lentgh - 66);
-        fileInfo.path[real_lentgh - 66] = '\0';
-        path_index = (int)(real_lentgh - 67);
-        while(fileInfo.path[path_index]) {
+        fileInfo.path = malloc((int) (real_lentgh - 66) * sizeof(char));
+        memcpy(fileInfo.path, &line[BEFORE_PATH_LENGTH], real_lentgh - BEFORE_PATH_LENGTH - 1);
+        fileInfo.path[real_lentgh - BEFORE_PATH_LENGTH - 1] = '\0';
+        path_index = (int) (real_lentgh - BEFORE_PATH_LENGTH - 2);
+        while (fileInfo.path[path_index]) {
             if (fileInfo.path[path_index] == ' ') {
                 fileInfo.path[path_index] = '/';
                 break;
             }
             path_index--;
         }
-        fileInfo.name = basename(fileInfo.path);
+        name = basename(fileInfo.path);
+        name_length = (int) strlen(name);
+        fileInfo.name = malloc(name_length * sizeof(char));
+        memcpy(fileInfo.name, &name[0], name_length);
+        fileInfo.name[name_length] = '\0';
         fileInfos[file_index] = fileInfo;
+        if (LOG) {
+            printf("----------------------------------------------------------------------------------------------------\n");
+            printf("file: %d\n", fileInfo.number);
+            printf("inode: %d\n", fileInfo.inode);
+            printf("permissions: %s\n", fileInfo.permissions);
+            printf("date: %-10lld\n", (long long) fileInfo.mtime);
+            printf("size: %d\n", fileInfo.size);
+            printf("path: %s\n", fileInfo.path);
+            printf("name: %s\n", fileInfo.name);
+        }
         file_index++;
-        printf("|%s|\n", line);
-        printf("|%d|\n", fileInfo.inode);
-        printf("|%s|\n", fileInfo.permissions);
-        printf("|%-10lld|\n", (long long) fileInfo.mtime);
-        printf("|%d|\n", fileInfo.size);
-        printf("|%s|\n", fileInfo.path);
-        printf("|%s|\n", fileInfo.name);
     }
-
+    if (LOG)
+        printf("----------------------------------------------------------------------------------------------------\n");
     free(line);
     fclose(file_ptr);
     return 0;
-
 }
 
-int compareFileContents(char* path1, char* path2) {
-    printf("Comparing file contents: %s, %s\n", path1, path2);
+int compareFileContents(struct FileInfo file1, struct FileInfo file2) {
     pid_t pid;
     int status;
     int link[2];
@@ -122,78 +142,90 @@ int compareFileContents(char* path1, char* path2) {
         dup2(link[1], STDOUT_FILENO);
         close(link[0]);
         close(link[1]);
-        execlp("diff", "diff", path1, path2, NULL);
+        execlp("diff", "diff", file1.path, file2.path, NULL);
     } else wait(&status);
-
     close(link[1]);
     int nbytes = (int) read(link[0], whatever, sizeof(whatever));
     if (nbytes == 0) {
-        printf("Same file contents.\n");
+        if (LOG) printf("Same file contents: %d, %d\n", file1.number, file2.number);
         return 0;
     }
-    printf("Different file contents.\n");
+    if (LOG) printf("Different file contents: %d, %d\n", file1.number, file2.number);
     return 1;
 }
 
 int compareFiles(struct FileInfo file1, struct FileInfo file2) {
-    if (strcmp(file1.name, file2.name) != 0) {                                                                          // different file names return 2;
-        printf("Different file names: %s, %s.\n", file1.path, file2.path);
+    if (file1.inode == file2.inode) {
+        if (LOG) printf("Same inodes: %d, %d\n", file1.number, file2.number);
+        return 3;                                                                                                       // same inodes return 3;
+    }
+    if (strcmp(file1.name, file2.name)) {                                                                               // different file names return 2;
+        if (LOG) printf("Different file names: %d, %d.\n", file1.number, file2.number);
         return 2;
     }
-    if (strcmp(file1.permissions, file2.permissions) != 0 || file1.size != file2.size) {                                // same file name, but different permissions or sizes, return 1
-        printf("Different permissions or sizes: %s, %s.\n", file1.path, file2.path);
+    if (strcmp(file1.permissions, file2.permissions)) {                                                                 // same file name, but different permissions, return 1
+        if (LOG) printf("Different permissions: %d, %d.\n", file1.number, file2.number);
         return 1;
     }
-    return compareFileContents(file1.path, file2.path);                                                                 // same contents return 0, different contents return 1, error return -1
+    if (file1.size != file2.size) {                                                                                     // same file name, same permissions, but different sizes, return 1
+        if (LOG) printf("Different sizes: %d, %d.\n", file1.number, file2.number);
+        return 1;
+    }
+    return compareFileContents(file1, file2);                                                                           // same contents return 0, different contents return 1, error return -1
 }
 
 int createHardLink(struct FileInfo file1, struct FileInfo file2) {
-    return 0;
+    int unlink_res = unlink(file2.path);
+    if (unlink_res) {
+        perror("Error unlinking file.\n");
+        return unlink_res;
+    }
+    int link_res = link(file1.path, file2.path);
+    if (link_res) perror("Error linking files.\n");
+    return link_res;
 }
 
 int main(int argc, char *argv[]) {
-
     if (argc != 2) {                                                                                                    // number of arguments must be 2
         fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
-
-    if (runlsdir(argv[1])) exit(EXIT_FAILURE);                                                                          // run lsdir and sort files.txt
-
-    FILE* file_ptr = fopen("files_sorted.txt", "r");
+    if (runlsdir(argv[1]))                                                                                              // run lsdir and sort files.txt
+        exit(2);
+    FILE *file_ptr = fopen("files_sorted.txt", "r");
     if (file_ptr == NULL) {
         perror("Error opening files_sorted.txt.\n");
-        exit(EXIT_FAILURE);
+        exit(3);
     }
     int numberFiles = getNumberFiles(file_ptr);                                                                         // determine number of files
-    struct FileInfo* fileInfos = malloc(numberFiles * sizeof(struct FileInfo));
-    if (getFileInfos(fileInfos, file_ptr)) exit(EXIT_FAILURE);                                                          // parse sorted file informations into array of structs
-
-    struct FileInfo currentFile = fileInfos[0];
-    struct FileInfo otherFile;
+    struct FileInfo *fileInfos = malloc(numberFiles * sizeof(struct FileInfo));
+    if (getFileInfos(fileInfos, file_ptr))                                                                              // parse sorted file informations into array of structs
+        exit(4);
     int i = 0;
     int j = 1;
     while (i < numberFiles - 1) {
-        while (j < numberFiles -1) {
-            otherFile = fileInfos[j];
-            int compare = compareFiles(currentFile, otherFile);
+        while (j < numberFiles) {
+            int compare = compareFiles(fileInfos[i], fileInfos[j]);
             switch (compare) {
-                case 2:
-                    j = numberFiles - 1;
-                    break;
-                case 1:
+                case 3:                                                                                                 // same inode, no need to create hardlink, already done
                     j++;
                     break;
-                case 0:
-                    createHardLink(currentFile, otherFile);
+                case 2:                                                                                                 // different names, no need to compare with the rest, since the files are sorted
+                    j = numberFiles;
+                    break;
+                case 1:                                                                                                 // different permissions, sizes or contents, move to the next file
                     j++;
                     break;
-                default:
+                case 0:                                                                                                 // same contents, create hardlink
+                    createHardLink(fileInfos[i], fileInfos[j]);                                                         // does not stop on hardlink error, continues to next file
+                    j++;
+                    break;
+                default:                                                                                                // does not stop on compare error, continues to next file
                     j++;
                     break;
             }
         }
-        currentFile = fileInfos[++i];
+        i++;
         j = i + 1;
     }
     exit(0);
