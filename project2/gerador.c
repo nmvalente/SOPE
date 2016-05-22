5932aabeac87a933300129b57e1e08262a20a6ab
francisquinha <francisquinha@gmail.com>
2016-05-22 16:20:33 +0100

    prepared log event

--- project2/gerador.c
+++ project2/gerador.c
@@ -12,7 +12,7 @@
 #define PROB_INT_0      5
 #define PROB_INT_1      8
 #define SEC2NANO        1000000000
-#define LOG "gerador.log"
+#define LOG_FILE        "gerador.log"
 
 /*
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; variavel global com inicializacao do mutex;
@@ -25,17 +25,26 @@ typedef enum {
     N, E, S, O
 } Acesso;
 
+typedef enum {
+    entrada, cheio, saida, fechado
+} Evento;
+
 struct Viatura {
     unsigned identificador;                                                                                             // unique identifier for vehicle
     unsigned tempo;                                                                                                     // parking time for vehicle
     Acesso acesso;                                                                                                      // park access for vehicle
+    clock_t start;                                                                                                      // start time of vehicle
+    clock_t start_gen;                                                                                                  // start time of generator
 };
 
-struct Viatura *create_viatura(unsigned identificador, unsigned tempo, Acesso acesso) {
+struct Viatura *create_viatura(unsigned identificador, unsigned tempo,
+                               Acesso acesso, clock_t start, clock_t start_gen) {
     struct Viatura *viatura = malloc(sizeof(struct Viatura));
     viatura->identificador = identificador;
     viatura->tempo = tempo;
     viatura->acesso = acesso;
+    viatura->start = start;
+    viatura->start_gen = start_gen;
     return viatura;
 }
 
@@ -67,15 +76,14 @@ void ticksleep(unsigned ticks, long ticks_seg) {                                
 }
 
 void *tracker_viatura(void *arg) {                                                                                      // ?? mudar nome disto e ainda não sei muito bem o que tem que fazer
-    pthread_t selfThread = pthread_self();
-    if(pthread_detach(selfThread) != 0){
-        perror("thread detached failed.\n");
-        exit(1);
+    if(pthread_detach(pthread_self()) != 0){
+        perror("thread detach failed\n");
+        exit(3);
     }
     struct Viatura *viatura = (struct Viatura *) arg;
     printf("tracker: id %u, t %u, a %u\n", viatura->identificador, viatura->tempo, viatura->acesso);
     free(viatura);
-    return NULL; 													// or pthread_exit(NULL);
+    return NULL; 													                                                    // or pthread_exit(NULL);
 };
 
 int main(int argc, char *argv[]) {
@@ -92,13 +100,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     long t_geracao_ticks = t_geracao * ticks_seg;
-    // create_MUTEX();
     srand((unsigned) time(NULL));
     unsigned identificador = 0;
     unsigned tempo;
     unsigned short pre_intervalo;
     unsigned intervalo;
     Acesso acesso;
+    // create_MUTEX();
     while (times(&now_tms) - start_time < t_geracao_ticks) {
         acesso = (Acesso) (rand() % N_ACESSOS);                                                                         // randomly get N, E, S or O for park access
         tempo = (rand() % N_TEMPOS + 1) * u_relogio;                                                                    // random parking time
@@ -107,26 +115,65 @@ int main(int argc, char *argv[]) {
         else if (pre_intervalo < PROB_INT_1) intervalo = u_relogio;
         else intervalo = 2 * u_relogio;
         ticksleep(intervalo, ticks_seg);                                                                                // sleep for random waiting period in clock ticks before generating vehicle
-        struct Viatura *viatura = create_viatura(identificador, tempo, acesso);                                         // create new vehicle
+        struct Viatura *viatura = create_viatura(identificador, tempo, acesso, times(&now_tms), start_time);            // create new vehicle
         printf("viatura: id %u, t %u, a %u\n", viatura->identificador, viatura->tempo, viatura->acesso);
         pthread_t thread_viatura;
         if (pthread_create(&thread_viatura, NULL, tracker_viatura, viatura) != 0) {                                     // create vehicle tracker thread
             printf("Error creating vehicle thread!");
             exit(2);
         }
-        pthread_detach(thread_viatura);
+        //pthread_detach(thread_viatura);
         identificador++;
     }
     return 0;
 }
 
-FILE* create_log_file() {
-    FILE* log;
-    if ((log = fopen(name,"w")) == NULL)
+FILE *create_log_file() {
+    FILE *log_file;
+    if ((log_file = fopen(LOG_FILE, "w")) == NULL)
         return NULL;
-
     fprintf(log_file, "t(ticks) ; id_viat ; destin ; t_estacion ; t_vida ; observ\n"); 					                // 1st row in the gerador.log file
-
     return log_file;
 }
 
+char* get_acesso(Acesso acesso) {
+    switch (acesso) {
+        case N:
+            return "N";
+        case E:
+            return "E";
+        case S:
+            return "S";
+        case O:
+            return "O";
+    }
+    return NULL;
+}
+
+char* get_evento(Evento evento) {
+    switch (evento) {
+        case entrada:
+            return "entrada";
+        case cheio:
+            return "cheio!";
+        case saida:
+            return "saida";
+        case fechado:
+            return "fechado!";
+    }
+    return NULL;
+}
+
+void log_event(FILE *log_file, struct Viatura *viatura, Evento evento) {
+    struct tms now_tms;
+    clock_t now = times(&now_tms);
+    fprintf(log_file, "%8d ; ", (int) (now - viatura->start_gen));
+    fprintf(log_file, "%7d ;      ", viatura->identificador);
+    fprintf(log_file, "%s ; ", get_acesso(viatura->acesso));
+    fprintf(log_file, "%10d ; ", viatura->tempo);
+    if (evento == saida)
+        fprintf(log_file, "%6d ; ", (int) (now - viatura->start));
+    else
+        fprintf(log_file, "     ? ; ");
+    fprintf(log_file, "%s\n", get_evento(evento));
+}
